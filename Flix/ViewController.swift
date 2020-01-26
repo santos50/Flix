e5e07ac3efad8ac1215df1e1f35837bd3751685e
Dianne Santos <santos50@purdue.edu>
2020-01-26 15:56:22 -0500

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Dianne Santos on 1/26/20.
+//  Copyright © 2020 codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
