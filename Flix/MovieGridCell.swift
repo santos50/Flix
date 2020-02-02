e1176beb9683c3afc7a71fe7a6766f0cf562fa7b
Dianne Santos <santos50@purdue.edu>
2020-02-02 08:30:31 -0500

    part 2 flix

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Dianne Santos on 2/1/20.
+//  Copyright © 2020 codepath. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
